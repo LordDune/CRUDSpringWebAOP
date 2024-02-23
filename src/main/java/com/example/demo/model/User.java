854ca19435ec02fcb59c47cde53c56c7fe65449b
Sergey LD <novatones1@gmail.com>
2024-02-24 02:00:45 +0500

    2

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class User {
 
@@ -10,4 +14,9 @@ public class User {
     private String firstName;
 
     private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
